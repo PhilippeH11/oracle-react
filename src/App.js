f17baca75f4bc461f305acb5ffaaba36279cf1b2
PhilippeH11 <phil31037@gmail.com>
2023-02-01 00:40:39 +0100

    Deploy app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DisplayImages from "./DisplayImages";
+
+function App() {
+  return (
+    <div className="container">
+      <DisplayImages />
+    </div>
+  );
+}
+
+export default App;
